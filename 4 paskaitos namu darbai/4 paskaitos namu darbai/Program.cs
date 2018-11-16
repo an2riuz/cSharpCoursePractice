d9d909e40d019ae54d713e9c849bbd466b61711d
an2riuz <an2riuz@gmail.com>
2018-11-16 11:39:57 +0200

    Sukurti funkciją kuri patikrina ar tekstas yra skaičius. Grąžina true jei teisingas.

--- 4 paskaitos namu darbai/4 paskaitos namu darbai/Program.cs
+++ 4 paskaitos namu darbai/4 paskaitos namu darbai/Program.cs
@@ -6,7 +6,25 @@ namespace _4_paskaitos_namu_darbai
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Iveskite skaiciu nuo -9 iki 9: ");
+            string skaicius = Console.ReadLine();
+            Console.WriteLine(ArSkaicius(skaicius));
+            Console.ReadKey();
+        }
+
+        static bool ArSkaicius(string skaicius)
+        {
+            int number;
+            bool yraSkaicius = Int32.TryParse(skaicius, out number);
+
+            if (yraSkaicius)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
