52d6042565d1d4ff98d40e8c93768d4cf15ec579
an2riuz <an2riuz@gmail.com>
2018-11-16 12:00:14 +0200

    Sukurti funkciją kuri konvertuoti string į int, patikrinti ar skaičius teisinguose rėžiuose: -9 ... 9

--- 4 paskaitos namu darbai/4 paskaitos namu darbai/Program.cs
+++ 4 paskaitos namu darbai/4 paskaitos namu darbai/Program.cs
@@ -9,6 +9,7 @@ namespace _4_paskaitos_namu_darbai
             Console.Write("Iveskite skaiciu nuo -9 iki 9: ");
             string skaicius = Console.ReadLine();
             Console.WriteLine(ArSkaicius(skaicius));
+            Console.WriteLine(KonvertuotiISkaiciu(skaicius));
             Console.ReadKey();
         }
 
@@ -25,6 +26,21 @@ namespace _4_paskaitos_namu_darbai
             {
                 return false;
             }
+        }
+        static string KonvertuotiISkaiciu(string skaicius)
+        {
+            int konvertuotas = Convert.ToInt32(skaicius);
+            if (konvertuotas > -9 && konvertuotas < 9)
+            {
+                string komentaras = "Skaicius yra teisinguose reziuose";
+                return komentaras;
+            }
+            else
+            {
+                string komentaras = "Skaicius yra neteisinguose reziuose";
+                return komentaras;
+            }
+
         }
     }
 }
