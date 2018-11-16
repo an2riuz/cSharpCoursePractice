f839e13426296bca09d812139c47a8270d5b5ade
an2riuz <an2riuz@gmail.com>
2018-11-16 15:08:11 +0200

    Sukurti funkciją, kuri ima int skaičių ir grąžina string - skaičių žodžiais

--- 4 paskaitos namu darbai/4 paskaitos namu darbai/Program.cs
+++ 4 paskaitos namu darbai/4 paskaitos namu darbai/Program.cs
@@ -10,6 +10,7 @@ namespace _4_paskaitos_namu_darbai
             string skaicius = Console.ReadLine();
             Console.WriteLine(ArSkaicius(skaicius));
             Console.WriteLine(KonvertuotiISkaiciu(skaicius));
+            Console.WriteLine("Ivestas skaicius " + SkaiciaiITeksta(Convert.ToInt32(skaicius)));
             Console.ReadKey();
         }
 
@@ -30,7 +31,7 @@ namespace _4_paskaitos_namu_darbai
         static string KonvertuotiISkaiciu(string skaicius)
         {
             int konvertuotas = Convert.ToInt32(skaicius);
-            if (konvertuotas > -9 && konvertuotas < 9)
+            if (konvertuotas > -10 && konvertuotas < 10)
             {
                 string komentaras = "Skaicius yra teisinguose reziuose";
                 return komentaras;
@@ -40,7 +41,64 @@ namespace _4_paskaitos_namu_darbai
                 string komentaras = "Skaicius yra neteisinguose reziuose";
                 return komentaras;
             }
-
+        }
+        static string SkaiciaiITeksta(int skaicius)
+        {
+            if (skaicius >= 0)
+            {
+                switch (skaicius)
+                {
+                    case 0:
+                        return "nulis";
+                    case 1:
+                        return "vienas";
+                    case 2:
+                        return "du";
+                    case 3:
+                        return "trys";
+                    case 4:
+                        return "keturi";
+                    case 5:
+                        return "penki";
+                    case 6:
+                        return "sesi";
+                    case 7:
+                        return "septyni";
+                    case 8:
+                        return "astuoni";
+                    case 9:
+                        return "devyni";
+                    case 10:
+                        return "desimt";
+                    case 11:
+                        return "vienuolika";
+                    case 12:
+                        return "dvylika";
+                    case 13:
+                        return "trylika";
+                    case 14:
+                        return "keturiolika";
+                    case 15:
+                        return "penkiolika";
+                    case 16:
+                        return "sesiolika";
+                    case 17:
+                        return "septyniolika";
+                    case 18:
+                        return "astuoniolika";
+                    case 19:
+                        return "devyniolika";
+                    default:
+                        return "Skaicius nera tarp -19 ir 19";
+                }
+            }
+            else
+            {
+                skaicius *= -1;
+                return "minus " + SkaiciaiITeksta(skaicius);
+            }
+                
+            
         }
     }
 }
